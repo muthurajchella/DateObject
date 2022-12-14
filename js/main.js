688e04e3bf6d0e3acfc89794d8f34f03d29c4fbf
Muthurajchella- <muthuraj.chella@gmail.com>
2022-12-14 19:19:04 +0530

    date Object

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+var date = new Date();
+// document.getElementById("para").innerHTML=date.toDateString();
+
+//toString
+// document.getElementById("para").innerHTML=date.toString();
+
+//toUTSString
+document.getElementById("para").innerHTML=date.toUTCString();
